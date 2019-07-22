d1bbae781c8c725c30ff6802fc6523004c810c78
Hendy <hendyracher@hotmail.com>
2019-07-22 12:41:44 +0100

    added empty Our.Umbraco.Look.AzureSearch.Tests project

---
+++ src/Our.Umbraco.Look.AzureSearch.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Our.Umbraco.Look.AzureSearch.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Our.Umbraco.Look.AzureSearch.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6696c5d6-0120-4699-b85f-084dfb0dd6d1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
