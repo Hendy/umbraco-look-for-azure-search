badb63cb504ac0fbed461cb39cf4d105fd054d14
Hendy <hendyracher@hotmail.com>
2019-07-22 14:12:01 +0100

    re-created Our.Umbraco.Look.AzureSearch csproj to fix path issue

--- src/Our.Umbraco.Look.AzureSearch/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.Look.AzureSearch/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ad2154a0-28e4-456f-8c68-e9f2acde3555")]
+[assembly: Guid("0ddec5fd-4922-4c0c-85c2-7ee4b2944eaf")]
 
 // Version information for an assembly consists of the following four values:
 //
