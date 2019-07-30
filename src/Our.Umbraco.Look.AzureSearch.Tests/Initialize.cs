c5b156acc0d1eadfa3bd804b283233882f2e1b10
Hendy <hendyracher@hotmail.com>
2019-07-30 09:57:21 +0100

    installed UmbracoCms.Core 7.3.0 into tests project, and made internals visible to it

---
+++ src/Our.Umbraco.Look.AzureSearch.Tests/Initialize.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Our.Umbraco.Look.AzureSearch.Services;
+
+namespace Our.Umbraco.Look.AzureSearch.Tests
+{
+    [TestClass]
+    public static class Initialize
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext testContext)
+        {
+            // TODO: always start with an empty index
+            //TestHelper.DeleteIndex();
+
+            // Wire up the location indexers
+            LookService.Initialize(null);
+        }
+    }
+}
